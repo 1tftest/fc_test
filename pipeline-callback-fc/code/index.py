6ca403d2d402196ce407658d9a4be199e023435e
jakae_bo@163.com <jake_bo@163.com>
2025-10-22 12:01:30 +0800

    Update index.py

--- pipeline-callback-fc/code/index.py
+++ pipeline-callback-fc/code/index.py
@@ -1,1 +1,15 @@
-test
+import json
+
+def handler(event, context):
+    print("Received event:", event)
+    
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+        'body': json.dumps({
+            'message': 'Hello from Simple Python Function!',
+            'success': True
+        })
+    }
